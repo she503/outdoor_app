6b6250991f4e7dd739e701acb9eff12caabe34af
qiaochong <qc@qiaochong.com>
2020-02-10 18:27:44 +0800

    update

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
 //    QGuiApplication app(argc, argv);
 
     QApplication app(argc, argv);
-    QTranslator trans;
-    if (!trans.load(":/language/translation_chinese.qm")) {
-        qDebug() << "faild to load translation qm !!!";
-    }
-    app.installTranslator(&trans);
+//    QTranslator trans;
+//    if (!trans.load(":/language/translation_chinese.qm")) {
+//        qDebug() << "faild to load translation qm !!!";
+//    }
+//    app.installTranslator(&trans);
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())

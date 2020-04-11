4adabb305552cb13a9963362a7c5bb0da5d43134
mmj <maminjun96@163.com>
2020-04-11 16:26:56 +0800

    debug版本，暂时禁用出现错误消息一直弹窗功能

--- ros_message_manager.cpp
+++ ros_message_manager.cpp
@@ -98,6 +98,7 @@ void RosMessageManager::parseMonitorMessageInfo(const QJsonObject &obj)
         error_list.push_back(temp_list);
         ++it;
     }
+    return;
 
     emit updateMonitorMessageInfo(error_list);
 }
